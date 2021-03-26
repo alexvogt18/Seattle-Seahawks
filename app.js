c7f730e8da9a1855225228dce30d2f74d34af65b
Alex Vogt <alexvogt18@gmail.com>
2021-03-25 23:28:38 -0700

    Completes Seahawks page up to variable interface.

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+function fanname(){
+
 let FanName = prompt('Welcome! What is your name?');
+
 document.write(FanName + ' is the biggest Seahawks fan on this site!');
+}
+
--- app2.js
+++ app2.js
@@ -11,3 +11,19 @@ let day = Math.floor(diff / oneDay);
 let footballStart = (228 - day);
 
 document.write(footballStart + ' Days until the first day of Seahawks Pre-Season!');
+
+function fanname(){
+
+    let FanName = prompt('Welcome! What is your name?');
+    
+    document.write(FanName + ' is the biggest Seahawks fan on this site!');
+    }
+
+    function securitycheck() {
+    
+    let SeahawksAlert = prompt('Would you happen to be a Seahawks fan?');
+ 
+    if (SeahawksAlert.toLowerCase() != 'yes') { 
+        alert('Stay Away!')
+    }
+}
