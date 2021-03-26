46f53b29009120b0f1b3b406b037b42ef74ada6f
Alex Vogt <alexvogt18@gmail.com>
2021-03-25 21:27:42 -0700

    2/3 JavaScript files working properly. Mid-way save time.

---
+++ app.js
@@ -0,0 +1,2 @@
+let FanName = prompt('Welcome! What is your name?');
+document.write(FanName + ' is the biggest Seahawks fan on this site!');
---
+++ app1.js
@@ -0,0 +1,5 @@
+let SeahawksAlert = prompt('Would you happen to be a Seahawks fan?')
+ 
+if (SeahawksAlert.toLowerCase() != 'yes') {
+    alert('Stay Away!')
+}
